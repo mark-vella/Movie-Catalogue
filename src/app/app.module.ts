a51454a0de12658d0168f6500c8fec2a019e8ef5
Mark Vella <markvella24@gmail.com>
2018-06-23 12:52:41 +0200

    Initial Commit: Engineer project architecture.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { MovieListComponent } from './movie-list/movie-list.component';
+import { MovieDetailComponent } from './movie-detail/movie-detail.component';
+import { MovieItemComponent } from './movie-list/movie-item/movie-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    MovieListComponent,
+    MovieDetailComponent,
+    MovieItemComponent
   ],
   imports: [
     BrowserModule
